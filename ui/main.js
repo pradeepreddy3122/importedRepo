cce370a3289ce9cbf497d8ff7a13d1011cd43e40
shalikasampath <shalikasampath@users.noreply.github.com>
2017-03-13 22:21:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var img = document.getElementById("madi");
+
+var margin = 0;
+
+function move() {
+    margin = margin + 10;
+    img.style.marginLeft=margin+'px';
+}
+
+img.onclick = function() {
+    var interval = setInterval(move,100);
+};
