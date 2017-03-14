8b8327180b2b48f84270c60c31ed191f85a35d77
shalikasampath <shalikasampath@users.noreply.github.com>
2017-03-14 21:58:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
  var counter = 0;
-app.get('/counter', function (req, res) {
+app.get('/count', function (req, res) {
 counter = counter +1;
 res.send(counter.toString());
   
