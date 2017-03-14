812404588798b6e8571c5d22d1098fbc80c411c0
shalikasampath <shalikasampath@users.noreply.github.com>
2017-03-14 21:36:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var button = document.getElementById("button");
 button.onClick = function() {
     var span = document.getElementById("count");
   counter = counter+1;
-  span.innerHTML = counter.toString;
+  span.innerHTML = counter.toString();
     
 };
