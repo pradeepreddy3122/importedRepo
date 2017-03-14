6956df9c774331757ce8518b1059d56ae2539f62
shalikasampath <shalikasampath@users.noreply.github.com>
2017-03-14 22:10:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 var counter = 0;
 
 var button = document.getElementById("butt");
-button.onclick = function() {
-    var span = document.getElementById("count");
-  counter = counter+1;
-  span.innerHTML = counter.toString();
+
+//create a request
+
+   var request = new XMLHTTPRequest();
+// capture response and store it in a variable
+
+request.onreadystatechange = function() {
+ 
+ if(request.readystate === XMLHTTPRequest.DONE){
+     // take some action
+     if(request.status === 200){
+         var counter = request.responseText;
+         var span = document.getElementById("count");
+         span.innerHTML = counter.toString();
+     }
+ }
     
 };
