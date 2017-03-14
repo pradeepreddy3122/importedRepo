eec4df579394eb3fe2eb2ff9f19051485d9f8a7b
shalikasampath <shalikasampath@users.noreply.github.com>
2017-03-14 21:58:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+ var counter = 0;
+app.get('/counter', function (req, res) {
+counter = counter +1;
+res.send(counter.toString());
+  
+});
+
 app.get('/:articleName',function(req,res) {
     var articleName = req.params.articleName;
    res.send(createTemp(articles[articleName]));
